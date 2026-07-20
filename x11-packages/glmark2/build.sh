84d0d1f500b430759f4955c496125481573929a6
lfdevs <lfdevs@lfdevs.com>
2026-07-20 21:46:56 +0800

    enhance(x11/glmark2): enable Wayland support

--- x11-packages/glmark2/build.sh
+++ x11-packages/glmark2/build.sh
@@ -3,12 +3,18 @@ TERMUX_PKG_DESCRIPTION="glmark2 is an OpenGL 2.0 and ES 2.0 benchmark"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2023.01
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/glmark2/glmark2/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8fece3fc323b643644a525be163dc4931a4189971eda1de8ad4c1712c5db3d67
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
-TERMUX_PKG_DEPENDS="libjpeg-turbo, libx11, opengl, libpng, libjpeg-turbo"
+TERMUX_PKG_DEPENDS="libjpeg-turbo, libx11, opengl, libpng, libjpeg-turbo, libwayland"
+TERMUX_PKG_BUILD_DEPENDS="libwayland-cross-scanner, libwayland-protocols"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--Dflavors=x11-gl,x11-glesv2,x11-gl-egl
+-Dflavors=x11-gl,x11-glesv2,x11-gl-egl,wayland-gl,wayland-glesv2
 "
+
+termux_step_pre_configure() {
+	termux_setup_wayland_cross_pkg_config_wrapper
+	export PATH="$TERMUX_PREFIX/opt/libwayland/cross/bin:$PATH"
+}
